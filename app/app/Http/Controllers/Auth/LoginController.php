7956c82ff4d9cf4cb96d95ae560984c630d31a78
massmediagroup-28 <b.zahurskyi@massmediagroup.pro>
2017-11-12 00:57:40 +0200

    update LoginController

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -30,10 +32,44 @@ class LoginController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        //$this->middleware('guest')->except('logout');
+    }
+
+    public function doLogin(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            //Login Successful
+            $response = ['success' => true];
+            //return a JSON response
+            return response()->json($response);
+        } else {
+            $response = ['success' => false, 'message' => 'Invalid login credentials'];
+
+            return response()->json($response);
+        }
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function doLogout(Request $request)
+    {
+        if (Auth::guest()) {
+            $response = ['success' => false];
+        } else {
+            $this->guard()->logout();
+
+            $response = ['success' => true];
+        }
+
+//        $request->session()->invalidate();
+
+        return response()->json($response);
     }
 }
